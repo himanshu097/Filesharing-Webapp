84bae08f013891f6ff31db61874607705c70fde1
himanshu097 <sonutelo123@gmail.com>
2020-12-04 17:33:59 +0530

    initial

---
+++ server.js
@@ -0,0 +1,67 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+const cors = require('cors');
+// Cors 
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(',')
+  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+}
+// import React from 'react'
+// console.log(React.version)
+// Default configuration looks like
+// {
+//     "origin": "*",
+//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     "preflightContinue": false,
+//     "optionsSuccessStatus": 204
+//   }
+
+app.use(cors(corsOptions))
+app.use(express.static('public'));
+
+const connectDB = require('./config/db');
+connectDB();
+
+app.use(express.json());
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+
+// Routes 
+app.get('/',(req,res)=>{
+  res.render('index.ejs');
+})
+app.use('/api/files', require('./routes/files'));
+app.use('/files', require('./routes/show'));
+app.use('/files/download', require('./routes/download'));
+app.listen(PORT, console.log(`Listening on port ${PORT}.`));
+
+//script.js
+const File = require('./models/file');
+const fs = require('fs');
+
+
+// Get all records older than 24 hours 
+async function fetchData() {
+    const files = await File.find({ createdAt : { $lt: new Date(Date.now() - 24*60*60*1000)} })
+    console.log(files.length);
+    if(files.length) {
+
+        for (const file of files) {
+            try {
+                fs.unlinkSync(file.path);
+                await file.remove();
+                console.log(`successfully deleted ${file.filename}`);
+            } catch(err) {
+                console.log(`error while deleting file ${err} `);
+            }
+        }
+        console.log('Job done!');
+    }
+    
+}
+fetchData();
